9a59faec8d62dccccb51975ec0342ca58b75ca4a
DomenicoSacco94 <domenico@element.in>
2025-03-21 09:47:57 +0100

    small fixes for the uml generator

--- src/main/java/generation/UMLGenerator.java
+++ src/main/java/generation/UMLGenerator.java
@@ -3,16 +3,13 @@ package generation;
 import com.thoughtworks.qdox.JavaProjectBuilder;
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaSource;
+import com.thoughtworks.qdox.model.JavaMethod;
 import net.sourceforge.plantuml.SourceStringReader;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Collection;
-
-//TODO REMOVE OBJECT FROM GENERATED DIAGRAMS
-//TODO REMOVE CLAS WITH THE MAIN METHOD
-//TODO DO NOT USE FULL PATH
 
 public class UMLGenerator {
 
@@ -26,7 +23,8 @@ public class UMLGenerator {
             StringBuilder umlContent = new StringBuilder("@startuml\n");
 
             for (JavaClass javaClass : source.getClasses()) {
-                if (javaClass.getName().equals("java.lang.Object")) {
+                // Skip classes with a main method and classes that extend Object
+                if (containsMainMethod(javaClass) || isObjectClass(javaClass)) {
                     continue;
                 }
 
@@ -36,15 +34,17 @@ public class UMLGenerator {
                     umlContent.append("class ").append(javaClass.getName()).append(" {\n");
                 }
 
-                javaClass.getFields().forEach(field -> umlContent.append("  ").append(field.getType().getFullyQualifiedName()).append(" ").append(field.getName()).append("\n"));
-                javaClass.getMethods().forEach(method -> umlContent.append("  ").append(method.getReturnType().getFullyQualifiedName()).append(" ").append(method.getName()).append("()\n"));
+                appendFields(umlContent, javaClass);
+                appendMethods(umlContent, javaClass);
                 umlContent.append("}\n");
 
-                if (javaClass.getSuperJavaClass() != null && !javaClass.getSuperJavaClass().getName().equals("java.lang.Object")) {
-                    umlContent.append(javaClass.getName()).append(" --|> ").append(javaClass.getSuperJavaClass().getName()).append("\n");
+                if (javaClass.getSuperJavaClass() != null && !isObjectClass(javaClass.getSuperJavaClass())) {
+                    umlContent.append(javaClass.getName()).append(" --|> ")
+                            .append(javaClass.getSuperJavaClass().getName()).append("\n");
                 }
 
-                javaClass.getInterfaces().forEach(interf -> umlContent.append(javaClass.getName()).append(" ..|> ").append(interf.getName()).append("\n"));
+                javaClass.getInterfaces().forEach(interf ->
+                        umlContent.append(javaClass.getName()).append(" ..|> ").append(interf.getName()).append("\n"));
             }
 
             umlContent.append("@enduml");
@@ -54,7 +54,7 @@ public class UMLGenerator {
                 outputDir.mkdirs();
             }
 
-            String fileName = source.getURL().getFile().replace(sourceDirPath, "").replace(File.separator, "_").replace(".java", "");
+            String fileName = new File(source.getURL().getFile()).getName().replace(".java", "");
             File umlFile = new File(outputDir, fileName + ".puml");
             try (FileWriter writer = new FileWriter(umlFile)) {
                 writer.write(umlContent.toString());
@@ -63,6 +63,31 @@ public class UMLGenerator {
             SourceStringReader reader = new SourceStringReader(umlContent.toString());
             reader.generateImage(new File(outputDir, fileName + ".png"));
         }
+    }
+
+    private void appendFields(StringBuilder umlContent, JavaClass javaClass) {
+        javaClass.getFields().forEach(field ->
+                umlContent.append("  ").append(field.getType().getFullyQualifiedName()).append(" ")
+                        .append(field.getName()).append("\n"));
+    }
+
+    private void appendMethods(StringBuilder umlContent, JavaClass javaClass) {
+        javaClass.getMethods().forEach(method ->
+                umlContent.append("  ").append(method.getReturnType().getFullyQualifiedName()).append(" ")
+                        .append(method.getName()).append("()\n"));
+    }
+
+    private boolean containsMainMethod(JavaClass javaClass) {
+        for (JavaMethod method : javaClass.getMethods()) {
+            if (method.getName().equals("main") && method.isStatic() && method.getParameters().size() == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isObjectClass(JavaClass javaClass) {
+        return javaClass.getName().equals("Object");
     }
 
     public static void main(String[] args) {
